3a2dd1e8755f483f019fe155a20674469fcd0f2e
makik <makiko.denda@outlook.jp>
2026-07-11 09:35:56 +0900

    fix mixin

--- fabric-example-mod-26.1.2/src/main/java/com/inari/inari/mixin/InariMixin.java
+++ fabric-example-mod-26.1.2/src/main/java/com/inari/inari/mixin/InariMixin.java
@@ -1,9 +1,17 @@
-package com.inari.inari.mixin;
+package com.inari.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.world.level.Level;
 
+/**
+ * InariMixin - Placeholder mixin for InariMining mod
+ * This will be used to inject mining automation logic in the future
+ */
 @Mixin(Level.class)
 public class InariMixin {
-    // TODO: 自動マイニング処理を追加
+    // TODO: Add mixin methods here for automatic mining functionality
+    // Examples:
+    // - Intercept block breaking
+    // - Handle item collection
+    // - Manage inventory updates
 }
