991f8d51868ff5e69a53cfbb8868a8b35ebac639
makik <makiko.denda@outlook.jp>
2026-07-11 09:33:23 +0900

    fix mixin

--- fabric-example-mod-26.1.2/src/main/java/com/inari/inari/InariMixin.java
+++
@@ -1,4 +0,0 @@
-package com.inari.inari;
-
-public class InariMixin {
-}
---
+++ fabric-example-mod-26.1.2/src/main/java/com/inari/inari/mixin/InariMixin.java
@@ -0,0 +1,9 @@
+package com.inari.inari.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.world.level.Level;
+
+@Mixin(Level.class)
+public class InariMixin {
+    // TODO: 自動マイニング処理を追加
+}
