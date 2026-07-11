9f1b1df847b70882ddd0e5009e829cd495ecd613
inari080 <makiko.denda@outlook.jp>
2026-07-11 10:01:14 +0900

    Fix: Simplify InariMiningClient and use InariMiningConfigData for ThemedGui

--- fabric-example-mod-26.1.2/src/client/java/com/inari/client/InariMiningClient.java
+++ fabric-example-mod-26.1.2/src/client/java/com/inari/client/InariMiningClient.java
@@ -1,14 +1,22 @@
 package com.inari.client;
 
+import com.inari.client.config.InariMiningConfigData;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * InariMiningクライアント側のエントリーポイント。
+ * InariMiningConfigを初期化してからInariMiningClientで
+ * 追加初期化（キーバインディング登録等）を実行する。
+ */
 public class InariMiningClient implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("inarimining");
+	public static final Logger LOGGER = LoggerFactory.getLogger("inarimining");
 
-    @Override
-    public void onInitializeClient() {
-        LOGGER.info("InariMiningClient initialized!");
-    }
+	@Override
+	public void onInitializeClient() {
+		// 設定データを初期化（InariMiningConfigDataが自動的にThemedGuiModで管理される）
+		LOGGER.info("InariMiningClient initialized!");
+		LOGGER.info("ThemedGui integration enabled - Press O to open settings");
+	}
 }
