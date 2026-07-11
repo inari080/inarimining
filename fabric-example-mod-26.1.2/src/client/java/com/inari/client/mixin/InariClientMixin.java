df0a62a2fcd02802a5641c1951924ec543f79d06
makik <makiko.denda@outlook.jp>
2026-07-11 09:39:05 +0900

    fix mixin

---
+++ fabric-example-mod-26.1.2/src/client/java/com/inari/client/mixin/InariClientMixin.java
@@ -0,0 +1,12 @@
+package com.inari.client.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.gui.screens.Screen;
+
+/**
+ * InariClientMixin - Client-side mixin for InariMining mod
+ */
+@Mixin(Screen.class)
+public class InariClientMixin {
+    // TODO: Add client-side mixin methods
+}
